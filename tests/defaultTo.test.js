f97bec5205b9e694aab34538af7f0ecdde8f2f36
Roni Hämäläinen <roni.hamalainen@tuni.fi>
2022-12-18 13:36:26 +0200

    Update unit tests

--- tests/defaultTo.test.js
+++ tests/defaultTo.test.js
@@ -1,6 +1,9 @@
 import defaultTo from "../src/defaultTo";
 
-// TODO: more tests
-
 test("maybe replace with default value", () => { expect(defaultTo(1, 10)).toBe(1); });
+test("maybe replace with default value", () => { expect(defaultTo(NaN, 10)).toBe(10); });
+test("maybe replace with default value", () => { expect(defaultTo(null, 10)).toBe(10); });
 test("maybe replace with default value", () => { expect(defaultTo(undefined, 10)).toBe(10); });
+test("maybe replace with default value", () => { expect(defaultTo([], 10)).toStrictEqual([]); });
+test("maybe replace with default value", () => { expect(defaultTo({}, 10)).toStrictEqual({}); });
+test("maybe replace with default value", () => { expect(defaultTo(Object(), 10)).toStrictEqual(Object()); });
--- tests/filter.test.js
+++ tests/filter.test.js
@@ -1,5 +1,16 @@
 import filter from "../src/filter";
 
-// TODO: more test cases
+const active_users = [
+    {"user": "a", "active": true},
+    {"user": "b", "active": true},
+]
 
-test("filter array elements with predicate", () => { expect(filter([{"user": "barney", "active": true}, {"user": "fred", "active": false}], ({active}) => active)).toStrictEqual([{"user": "barney", "active": true}]); });
+const inactive_users = [
+    {"user": "c", "active": false},
+]
+
+const users = active_users.concat(inactive_users)
+
+test("filter array elements with predicate", () => { expect(filter(users, ({active}) => active)).toStrictEqual(active_users); });
+test("filter array elements with predicate", () => { expect(filter(users, ({active}) => !active)).toStrictEqual(inactive_users); });
+test("filter array elements with predicate", () => { expect(filter(users, ({user}) => user === "d")).toStrictEqual([]); });
